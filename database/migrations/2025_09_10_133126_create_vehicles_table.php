74639dc72d1bc42f9dedd0e004c1c5258242437c
algmdev <alexisgomez1722@gmail.com>
2025-09-10 09:46:36 -0500

    Migraciones de las tablas marcas, modelos, clientes y vehículos

---
+++ database/migrations/2025_09_10_133126_create_vehicles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('plate_number', 6);
+            $table->integer('year');
+            $table->foreignId('models_id')->references('id')->on('models')->onDelete('restrict');
+            $table->foreignId('clients_id')->references('id')->on('clients')->onDelete('restrict');
+            $table->foreignId('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
